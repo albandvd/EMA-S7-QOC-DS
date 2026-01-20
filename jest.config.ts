43ff8242f71274623e9402e1517ec738d0b290b8
albandvd <alban.david06@gmail.com>
2026-01-20 16:17:41 +0100

    Fix: delete integration test

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/test'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
 };
